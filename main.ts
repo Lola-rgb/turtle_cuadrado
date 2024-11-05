950a263165796bcc997d44dfcd75c06f7977b4f6
Lola-rgb <153564317+Lola-rgb@users.noreply.github.com>
2024-11-05 13:31:13 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 4; index++) {
+        turtle.forward(1)
+        turtle.turnRight()
+        turtle.pen(TurtlePenMode.Down)
+    }
+})
